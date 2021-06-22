ac2d5d5ce0f601ac460313d46da6bd2ad9db7633
Ashwin <ashwin.dinesh01@gmail.com>
2021-06-22 16:52:25 +0530

    Add MainViewModel unit tests

--- app/src/test/java/com/ashwin/android/observalefield/MainViewModelTest.kt
+++ app/src/test/java/com/ashwin/android/observalefield/MainViewModelTest.kt
@@ -1,10 +1,36 @@
 package com.ashwin.android.observalefield
 
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 class MainViewModelTest {
+    lateinit var mainViewModel: MainViewModel
+
+    @Before
+    fun setup() {
+        mainViewModel = MainViewModel()
+    }
+
     @Test
     fun testUpdateUser() {
+        // Trigger
+        val user = User()
+        user.name = "John Doe"
+        user.email = "john.doe@email.com"
+        mainViewModel.updateUser(user)
 
+        // Validate
+        assertEquals(user, mainViewModel.userField.get())
+    }
+
+    @Test
+    fun testupdateBoolean() {
+        // Trigger
+        val b = true
+        mainViewModel.updateBoolean(b)
+
+        // Validate
+        assertEquals(b, mainViewModel.boolField.get())
     }
 }
